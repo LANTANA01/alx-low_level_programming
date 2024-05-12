d633c0afad8597243c7962539aee865bf48acd15
LANTANA01 <ojimahtijani@gmail.com>
2024-05-11 22:11:44 -0700

    Big_O_notation

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - This searches for a value in an array of
+ * integers using the Linear search algorithm O(1)
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: Exit success
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value [%u] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+0x1E-search_algorithms: The big O notations/Time complexity.
